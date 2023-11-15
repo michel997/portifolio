55efef29f3ed6220239ec8eeb2e36c81febd8bfc
Michel Lima <michellimamonteiro@hotmail.com>
2023-11-15 13:03:46 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/michel997.png" />
-    <Titulo fontSize={20}>Michel Lima</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Michel Lima</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        michel997
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
